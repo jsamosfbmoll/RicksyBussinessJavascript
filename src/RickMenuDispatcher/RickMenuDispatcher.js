6a675cafdcd077776e57a6da5a7999fb2988cb76
Jose Maria Samos <josemasamos@gmail.com>
2020-11-28 16:58:16 +0100

    refactor: put RickMenuDispatcher in his own file
    Split RickMenuDispatcher in his own package

---
+++ src/RickMenuDispatcher/RickMenuDispatcher.js
@@ -0,0 +1,16 @@
+function RickMenuDispatcher() {
+    this.stock = 100;
+    this.cost = 10;
+}
+
+RickMenuDispatcher.prototype.toString = function() {
+    return "Stock: " + this.stock + " cost: " + this.cost;
+}
+
+RickMenuDispatcher.prototype.dispatch = function(card) {
+    if (this.stock > -1 && card.pay(this.cost)) {
+        this.stock -= 1;
+        return true;
+    }
+    return false;
+}
