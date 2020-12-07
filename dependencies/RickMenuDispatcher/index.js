ed0335cd3923362f791b6add70fa1d3954e69901
Jose Maria Samos <josemasamos@gmail.com>
2020-12-07 19:25:54 +0100

    feat: add getters to RickMenuDispatcher

--- dependencies/RickMenuDispatcher/index.js
+++ dependencies/RickMenuDispatcher/index.js
@@ -1,14 +1,22 @@
-function RickMenuDispatcher() {
-    this.stock = 100;
-    this.cost = 10;
+function RickMenuDispatcher(stock, cost) {
+    this.stock = stock;
+    this.cost = cost;
 }
 
 RickMenuDispatcher.prototype.toString = function() {
     return "Stock: " + this.stock + " cost: " + this.cost;
 }
 
+RickMenuDispatcher.prototype.getCost = function() {
+    return this.cost;
+}
+
+RickMenuDispatcher.prototype.getStock = function() {
+    return this.stock;
+}
+
 RickMenuDispatcher.prototype.dispatch = function(card) {
-    if (this.stock > -1 && card.pay(this.cost)) {
+    if (this.stock > 0 && card.pay(this.cost)) {
         this.stock -= 1;
         return true;
     }
